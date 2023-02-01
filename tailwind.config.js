405e097237b31f8efd63485689c7b19c53364ffe
ucinhow <ucinhow@gmail.com>
2023-02-01 10:55:38 +0800

    chore: project initial & install dependence

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+  // theme: {
+  //   extend: {},
+  // },
+  plugins: [require("daisyui")],
+  daisyui: { themes: ["winter", "business"], darkTheme: "business" },
+};
